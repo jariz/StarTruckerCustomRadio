54c99e28179a18277c215786374d693ee6d7af31
Jari Z <jarizw@gmail.com>
2024-09-21 01:47:45 +0200

    Use streaming instead of loading entire PCM data to memory. Need to fix END REACHED bug.

--- StarTruckerCustomRadio/Core.cs
+++ StarTruckerCustomRadio/Core.cs
@@ -1,4 +1,5 @@
 ﻿using Il2Cpp;
+using Il2CppInterop.Runtime;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using MelonLoader;
 using NAudio.Wave;
@@ -15,17 +16,21 @@ namespace StarTruckerCustomRadio
     public class TrackInfo
     {
         public Tag Tag;
+        public string Path;
 
-        //private StreamedAudioClip streamedAudioClip;
         private AudioClip audioClip;
-        private string path;
 
+        private int sampleRate = 44100;
+        private int channels = 2; // Stereo
+
+        private Mp3FileReader mp3Reader;
+        private WaveStream waveStream;
 
         public TrackInfo(string path)
         {
             var tagFile = TagLib.File.Create(path);
             Tag = tagFile.Tag;
-            this.path = path;
+            this.Path = path;
             //streamedAudioClip = new StreamedAudioClip(path);
         }
 
@@ -43,63 +48,16 @@ namespace StarTruckerCustomRadio
 
         public AudioClip GetAudioClip()
         {
+            mp3Reader = new Mp3FileReader(Path);
+            waveStream = WaveFormatConversionStream.CreatePcmStream(mp3Reader);
 
-            var mp3Reader = new Mp3FileReader(path);
-            var waveStream = WaveFormatConversionStream.CreatePcmStream(mp3Reader);
-            byte[] buffer = new byte[waveStream.Length];
-            waveStream.Read(buffer, 0, buffer.Length);
+            var readerCallback = DelegateSupport.ConvertDelegate<AudioClip.PCMReaderCallback>(this.OnAudioRead);
+            var posCallback = DelegateSupport.ConvertDelegate<AudioClip.PCMSetPositionCallback>(this.OnAudioSetPosition);
 
-            // Create an AudioClip from WAV data
-            audioClip = AudioClip.Create("LoadedMP3", buffer.Length / 2, 2, 44100, false);
-            audioClip.SetData(ConvertByteToFloatArray(buffer), 0);
+            audioClip = AudioClip.Create("streamy", (int)waveStream.Length / 2, channels, sampleRate, true, readerCallback, posCallback);
             return audioClip;
         }
-        
-    }
 
-    public class StreamedAudioClip
-    {
-        private int sampleRate = 44100;
-        private int channels = 2; // Stereo
-        private Mp3FileReader mp3Reader;
-        private WaveStream waveStream;
-
-        private AudioClip audioClip;
-        public AudioClip AudioClip { get { return audioClip; } }
-
-        public StreamedAudioClip(string path)
-        {
-            //// TODO: support non-mp3 formats
-            //mp3Reader = new Mp3FileReader(path);
-            //waveStream = WaveFormatConversionStream.CreatePcmStream(mp3Reader);
-
-            //audioClip = AudioClip.Create("streamy", sampleRate, sampleRate * channels, channels, true, (AudioClip.PCMReaderCallback)OnAudioRead, (AudioClip.PCMSetPositionCallback)OnAudioSetPosition);
-
-
-            // Use NAudio to load the MP3 and convert it to WAV
-            mp3Reader = new Mp3FileReader(path);
-            waveStream = WaveFormatConversionStream.CreatePcmStream(mp3Reader);
-            byte[] buffer = new byte[waveStream.Length];
-            waveStream.Read(buffer, 0, buffer.Length);
-
-            // Create an AudioClip from WAV data
-            audioClip = AudioClip.Create("LoadedMP3", buffer.Length / 2, 2, 44100, false);
-            audioClip.SetData(ConvertByteToFloatArray(buffer), 0);
-        }
-
-        private float[] ConvertByteToFloatArray(byte[] input)
-        {
-            // Convert WAV byte data to float array
-            int len = input.Length / 2;
-            float[] result = new float[len];
-            for (int i = 0; i < len; i++)
-            {
-                result[i] = (short)(input[i * 2] | input[i * 2 + 1] << 8) / 32768f;
-            }
-            return result;
-        }
-
-        // Callback method: Unity requests more audio data for the streaming clip
         void OnAudioRead(Il2CppStructArray<float> data)
         {
             int samplesToRead = data.Length;
@@ -122,6 +80,7 @@ namespace StarTruckerCustomRadio
                 // If less data was read than requested, fill the rest with zeros
                 if (bytesRead < byteBuffer.Length)
                 {
+                    Melon<Core>.Logger.Warning($"END REACHED, {bytesRead} < {byteBuffer.Length}");
                     for (int i = bytesRead / 2; i < data.Length; i++)
                     {
                         data[i] = 0f;
@@ -138,9 +97,58 @@ namespace StarTruckerCustomRadio
         // Callback method: Unity adjusts the playback position (e.g., seeking)
         void OnAudioSetPosition(int newPosition)
         {
+
+            Melon<Core>.Logger.Msg("setPos " + newPosition);
             waveStream.Position = newPosition * channels * 2; // 2 bytes per sample (16-bit PCM)
         }
+
     }
+
+    //public class StreamedAudioClip
+    //{
+    //    private int sampleRate = 44100;
+    //    private int channels = 2; // Stereo
+    //    private Mp3FileReader mp3Reader;
+    //    private WaveStream waveStream;
+
+    //    private AudioClip audioClip;
+    //    public AudioClip AudioClip { get { return audioClip; } }
+
+    //    public StreamedAudioClip(string path)
+    //    {
+    //        //// TODO: support non-mp3 formats
+    //        //mp3Reader = new Mp3FileReader(path);
+    //        //waveStream = WaveFormatConversionStream.CreatePcmStream(mp3Reader);
+
+    //        //audioClip = AudioClip.Create("streamy", sampleRate, sampleRate * channels, channels, true, (AudioClip.PCMReaderCallback)OnAudioRead, (AudioClip.PCMSetPositionCallback)OnAudioSetPosition);
+
+
+    //        // Use NAudio to load the MP3 and convert it to WAV
+    //        mp3Reader = new Mp3FileReader(path);
+    //        waveStream = WaveFormatConversionStream.CreatePcmStream(mp3Reader);
+    //        byte[] buffer = new byte[waveStream.Length];
+    //        waveStream.Read(buffer, 0, buffer.Length);
+
+    //        // Create an AudioClip from WAV data
+    //        audioClip = AudioClip.Create("LoadedMP3", buffer.Length / 2, 2, 44100, false);
+    //        audioClip.SetData(ConvertByteToFloatArray(buffer), 0);
+    //    }
+
+    //    private float[] ConvertByteToFloatArray(byte[] input)
+    //    {
+    //        // Convert WAV byte data to float array
+    //        int len = input.Length / 2;
+    //        float[] result = new float[len];
+    //        for (int i = 0; i < len; i++)
+    //        {
+    //            result[i] = (short)(input[i * 2] | input[i * 2 + 1] << 8) / 32768f;
+    //        }
+    //        return result;
+    //    }
+
+    //    // Callback method: Unity requests more audio data for the streaming clip
+        
+    //}
 
     public class Core : MelonMod
     {
@@ -173,10 +181,15 @@ namespace StarTruckerCustomRadio
             {
                 var song = new SongDescription();
                 song.name = $"{item.Tag.Title} - {item.Tag.JoinedPerformers}";
+                if (item.Tag.IsEmpty)
+                {
+                    song.name = Path.GetFileName(item.Path);
+                }
+                
                 song.artistNameStringId = $"STR_CUSTOMTRACK_{index}_TITLE";
                 song.songNameStringId = $"STR_CUSTOMTRACK_{index}_ARTIST";
-                StringTable.stringTable.Add(song.artistNameStringId, item.Tag.Title);
-                StringTable.stringTable.Add(song.songNameStringId, item.Tag.JoinedPerformers);
+                StringTable.stringTable.TryAdd(song.artistNameStringId, item.Tag.Title);
+                StringTable.stringTable.TryAdd(song.songNameStringId, item.Tag.JoinedPerformers);
                 song.audioClip = item.GetAudioClip();
                 song.audioClipInstrumental = item.GetAudioClip(); // No idea if this is used but better be sure
                 list.Add(song);
--- StarTruckerCustomRadio/Patch.cs
+++ StarTruckerCustomRadio/Patch.cs
@@ -1,21 +1,43 @@
 ﻿using HarmonyLib;
 using Il2Cpp;
 using MelonLoader;
+using System.Diagnostics;
 using System.Text;
 using UnityEngine;
 
 namespace StarTruckerCustomRadio
 {
+    [HarmonyPatch(typeof(RadioStationInstance), nameof(RadioStationInstance.EnqueueNextPlaylistItem))]
+    public static class RadioStationInstancePatch
+    {
+        static void Postfix(RadioStationInstance __instance)
+        {
+            Melon<Core>.Logger.Msg($"-------PLAYLIST---------");
+            Melon<Core>.Logger.Msg($"CURR INDEX: {__instance.CurrentPlaylistEntryIdx}");
+            foreach (var entry in __instance.Playlist)
+            {
+                
+                Melon<Core>.Logger.Msg($"-- DESC: {entry.itemDesc.name}");
+                Melon<Core>.Logger.Msg($"-- TYPE: {entry.itemType}");
+                Melon<Core>.Logger.Msg($"-- DRTN: {entry.duration}");
+                Melon<Core>.Logger.WriteSpacer();
+            }
+            Melon<Core>.Logger.Msg($"------------------------");
+        }
+    }
+
 
     [HarmonyPatch(typeof(HiFi), nameof(HiFi.Init))]
     public static class HiFiPatch
     {
-        private static void Prefix(HiFi __instance)
+        static void Prefix(HiFi __instance)
         {
             try
             {
-                StringTable.stringTable.Add(Core.customRadioNameStringId, "CustomRadio");
-                StringTable.stringTable.Add(Core.customRadioFreqStringId, "Your harddrive");
+                StringTable.stringTable.TryAdd(Core.customRadioNameStringId, "CustomRadio");
+                StringTable.stringTable.TryAdd(Core.customRadioFreqStringId, "Your harddrive");
+
+                var goldRock = __instance.m_radioStationDef;
 
                 var radioStation = new RadioStationDescription
                 {
@@ -23,14 +45,16 @@ namespace StarTruckerCustomRadio
                     stationFreqStringId = Core.customRadioFreqStringId,
                     songs = Melon<Core>.Instance.GetSongDescriptions(),
                     adverts = new Il2CppSystem.Collections.Generic.List<RadioAdvertDescription>(),
-                    stings = new Il2CppSystem.Collections.Generic.List<RadioStingDescription>()
+                    stings = new Il2CppSystem.Collections.Generic.List<RadioStingDescription>(),
+                    //adverts = goldRock.adverts,
+                    //stings = goldRock.stings,
                 };
 
                 __instance.m_radioStationDef = radioStation;
             }
             catch (Exception ex)
             {
-                Melon<StarTruckerCustomRadio.Core>.Logger.Error(ex);
+                Melon<Core>.Logger.Error(ex);
             }
 
 
